0ea3372cb0ca6de5cc2b4f6fffc57924e0b0e756
safdar211ali <safdar211@hotmail.com>
2024-06-03 21:46:57 +0500

    updated

--- garments_app/events/bom.py
+++ garments_app/events/bom.py
@@ -4,7 +4,10 @@ from frappe.utils import cint, cstr
 
 
 def bom_validation_for_percentage_fields(self, method):
-	if self.items:
-		ratio_sum = cint(sum(item.ratio for item in self.items if item.ratio))
-		if ratio_sum != 100:
-			frappe.throw(_('Sum of ratio is {0} it should be equal to 100').format(cstr(ratio_sum)))
+    if self.items:
+        # Check if ratio field exists in BOMItem objects
+        if hasattr(self.items[0], 'ratio'):
+            ratio_sum = cint(sum(item.ratio for item in self.items if item.ratio))
+            if ratio_sum != 100:
+                frappe.throw(_('Sum of ratio is {0}, it should be equal to 100').format(cstr(ratio_sum)))
+
