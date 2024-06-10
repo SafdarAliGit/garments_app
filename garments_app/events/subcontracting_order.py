02ec430823ce9fff6771c51c355dab987183cc33
safdar211ali <safdar211@hotmail.com>
2024-06-10 22:11:17 +0500

    updated

--- garments_app/events/subcontracting_order.py
+++ garments_app/events/subcontracting_order.py
@@ -6,8 +6,8 @@ def calculate_total_reqiured_qty_and_no_bags(self, doc):
 		total_required_qty = 0
 		total_no_of_bags = 0
 		for supplied_item in self.get('supplied_items'):
-			total_required_qty += supplied_item.required_qty
-			total_no_of_bags += supplied_item.bags
+			total_required_qty += supplied_item.required_qty if supplied_item.required_qty else 0
+			total_no_of_bags += supplied_item.bags if supplied_item.bags else 0
 
 		self.total_required_qty = total_required_qty
 		self.total_no_of_bags = total_no_of_bags
