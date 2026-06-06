0f6a20ec530f0a2a4beb36501ef5c05ee614c94d
SafdarAliGit <safdar211@gmail.com>
2026-06-06 16:45:35 +0500

    updated

--- garments_app/garments_app/doctype/inquiry_cost_sheet_garment/inquiry_cost_sheet_garment.js
+++ garments_app/garments_app/doctype/inquiry_cost_sheet_garment/inquiry_cost_sheet_garment.js
@@ -393,7 +393,8 @@ frappe.ui.form.on('Job Costing Accessory', {
     var d = locals[cdt][cdn];
     let pcs_per_ctn = d.pcs_per_ctn || 0;
     let unit_per_default_uom = d.unit_per_default_uom || 0;
-    if (pcs_per_ctn > 0 && unit_per_default_uom > 0) {
+    let item_group = d.item_group || "";
+    if (pcs_per_ctn > 0 && unit_per_default_uom > 0 && item_group === "CARTON") {
         frappe.model.set_value(cdt, cdn, 'qty', flt(pcs_per_ctn / unit_per_default_uom));
     }
     }
