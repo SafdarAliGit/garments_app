1cc909829f260a1795ffe1bfae68caec91a5cdef
SafdarAliGit <safdar211@gmail.com>
2026-06-17 18:50:29 +0500

    updated

--- garments_app/public/js/item.js
+++ garments_app/public/js/item.js
@@ -7,6 +7,13 @@ frappe.ui.form.on('Item', {
                 ]
             };
         });
+        frm.set_query("item_group", function() {
+            return {
+                filters: [
+                    ["Item Group", "name", "not in", ["Products", "SKU Items"]]
+                ]
+            };
+        });
     }
 });
 
