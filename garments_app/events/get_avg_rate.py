f8a254eea27e3562eba20586672c972e5429e389
SafdarAliGit <safdar211@gmail.com>
2026-08-06 16:27:18 +0500

    updated

--- garments_app/events/get_avg_rate.py
+++ garments_app/events/get_avg_rate.py
@@ -9,7 +9,6 @@ def get_avg_rate(item_code):
         select avg(price_list_rate)
         from `tabItem Price`
         where item_code = %(item_code)s
-          and price_list = 'Standard Buying'
           and price_list_rate > 0
     """, {"item_code": item_code})
 
